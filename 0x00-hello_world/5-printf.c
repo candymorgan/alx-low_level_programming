62bff2636187b66551a595dfe3c7d8848870c31a
candymorgan <candyaz1000@gmail.com>
2022-04-07 14:39:58 -0700

    zeh corr

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - print the string in the put finction 
- *
  * Description: using the main function
  * this program prints "programming is like building a multilingual puzzle"
  * Return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
