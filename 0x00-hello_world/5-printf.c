9c6aee76d3020e8f175bbab9362c5a924065003e
candymorgan <candyaz1000@gmail.com>
2022-04-07 14:50:49 -0700

    nawa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - print the string in the put finction 
+ *
  * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle"
+ *i this program prints "programming is like building a multilingual puzzle"
  * Return: 0
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
