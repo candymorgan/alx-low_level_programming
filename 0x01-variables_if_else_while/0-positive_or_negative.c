1e63e19f0858c35752e6063d5073da1a4c2bf7ef
candymorgan <candyaz1000@gmail.com>
2022-04-08 05:48:39 -0700

    positive c4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,8 +15,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	  /* your code goes there */
-        if (n > 0)
+	  /* your code goes there */	
+if (n > 0)
 	{
 			printf("%i is positive\n", n);
 	}
