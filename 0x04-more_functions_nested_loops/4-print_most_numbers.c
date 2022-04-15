08b68c3ff06d7a97f55c2c5cc1899a8d9831607e
candymorgan <candyaz1000@gmail.com>
2022-04-14 19:28:13 -0700

    print most numbers3

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,13 @@
 void print_most_numbers(void)
 {
 char c = '0';
-
 while (c <= '9')
 {
 if (c != '2' && c != '4')
-										{										_putchar(c);									}
-c++;	
+{
+_putchar(c);									
+}
+c++;
 }
 _putchar('\n');
 }
