0654d2a6a1dce53a2c65256d68f83331a920a7ba
candymorgan <candyaz1000@gmail.com>
2022-04-14 19:05:31 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_most_numbers - check the code for Holberton School students.
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+int c = '0';
+while (c <= '9')
+{
+if (c != '2' && c != '4')
+{
+_putchar(c);									}						
+c++;										}
+_putchar('\n');
+}
