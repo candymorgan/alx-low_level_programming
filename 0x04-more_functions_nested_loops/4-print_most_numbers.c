3177451f638c83d28f39c81ada1c0dbf5cbab954
candymorgan <candyaz1000@gmail.com>
2022-04-14 20:33:40 -0700

    this is 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
+
 /**
-* print_most_numbers - check the code for Holberton School students.
-* Return: Always 0.
+* print_most_numbers - prints the numbers, from 0 to 9,
+* except 2 and 4, followed by a new line
 */
 void print_most_numbers(void)
 {
-char c = '0';
-while (c <= '9')
+int i;
+for (i = 0; i < 10; i++)
 {
-if (c != '2' && c != '4')
-{
-_putchar(c + '0');								}
-c++;
+if (i != 2 && i != 4)
+{										_putchar(i + '0');								}
 }
 _putchar('\n');
 }
