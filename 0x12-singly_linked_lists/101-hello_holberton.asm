f8834b824b3f93064b1ae12cab2f7ec4ece04ab4
candymorgan <candyaz1000@gmail.com>
2022-05-24 13:18:49 -0700

    last task commit

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("(Hello, Holberton)\n");
+	return (0);
+}
